63aead00fbecafb0ffdfe7ce7afcf8e91a9f7dfb
Will Young <wil.wyo@gmail.com>
2020-03-23 16:32:13 -0400

    Add Stack data structure

---
+++ src/main/kotlin/cafe/dragons/tgtk/collections/Stack.kt
@@ -0,0 +1,12 @@
+package cafe.dragons.tgtk.collections
+
+interface Stack<T> {
+    fun push(elem: T): T?
+    fun pop(): T?
+    fun peek(): T? {
+        val elem = pop()
+        if (elem != null) push(elem)
+        return elem
+    }
+    fun clear()
+}
